94604fbe897eaece9e8fba699c878ec9c75806ca
khanak1 <144924515+khanak1@users.noreply.github.com>
2024-10-18 20:13:44 +0500

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -5,23 +5,23 @@ require("dotenv").config();
 const config = {
   SESSION_ID: process.env.SESSION_ID || "Sarkar-Ak&Xjw1PM1U",
   PREFIX: process.env.PREFIX || '.',
-  AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== true ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
-  AUTO_DL: process.env.AUTO_DL !== true ? process.env.AUTO_DL === 'true' : true,
-  AUTO_READ: process.env.AUTO_READ !== false ? process.env.AUTO_READ === 'true' : true,
-  AUTO_TYPING: process.env.AUTO_TYPING !== false ? process.env.AUTO_TYPING === 'true' : true,
-  AUTO_RECORDING: process.env.AUTO_RECORDING !== false ? process.env.AUTO_RECORDING === 'true' : true,
-  ALWAYS_ONLINE: process.env.ALWAYS_ONLINE !== false ? process.env.ALWAYS_ONLINE === 'true' : true,
-  AUTO_REACT: process.env.AUTO_REACT !== true ? process.env.AUTO_REACT === 'true' : false,
+  AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || true,
+  AUTO_DL: process.env.AUTO_DL || true,
+  AUTO_READ: process.env.AUTO_READ || false,
+  AUTO_TYPING: process.env.AUTO_TYPING || false,
+  AUTO_RECORDING: process.env.AUTO_RECORDING || false,
+  ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || false,
+  AUTO_REACT: process.env.AUTO_REACT || true,
    /*auto block only for 212 */
-  AUTO_BLOCK: process.env.AUTO_BLOCK !== false ? process.env.AUTO_BLOCK === 'false' : true,
+  AUTO_BLOCK: process.env.AUTO_BLOCK || false,
   
   
-  NOT_ALLOW: process.env.NOT_ALLOW !== true ? process.env.NOT_ALLOW === 'true' : true,
+  NOT_ALLOW: process.env.NOT_ALLOW || true,
   MODE: process.env.MODE || "public",
   OWNER_NAME: process.env.OWNER_NAME || "✪⏤͟͞★⃝ꪶ‎AFZAAL~KHAN𖥘✪͜͡➺",
   OWNER_NUMBER: process.env.OWNER_NUMBER || "923556159234",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
-  WELCOME: process.env.WELCOME !== true ? process.env.WELCOME === 'true' : false, 
+  WELCOME: process.env.WELCOME || true, 
 };
 
 
